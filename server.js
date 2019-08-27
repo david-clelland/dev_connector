e2b3088c7247edb838a92b6ea3cc0c348c1f53aa
David <davidclelland6@gmail.com>
2019-08-27 13:23:54 -0400

    added routes api files for auth, posts, profile, users

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ connectDB();
 
 // calling app get request to send message
 app.get('/', (req, res) => res.send('API Running'));
-
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 // set up port to run
 const PORT = process.env.PORT || 5000;
 
