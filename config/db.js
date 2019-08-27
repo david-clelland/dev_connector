a158d3bfa3640b4b8f2ff144ecc4e480353a0cec
David <davidclelland6@gmail.com>
2019-08-27 13:07:24 -0400

    db.js comments - research async and await

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
+//  bring in and require Mongoose to connect to MongoDB
 const mongoose = require('mongoose');
+
 const config = require('config');
+// set variable db as mongoURI string in default.json file
 const db = config.get('mongoURI');
 
+// TODO reseaarch further async and await and comment below function
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
