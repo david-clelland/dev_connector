ec4f81635e95cd8db68f2157e13db4a8c3b83d61
David <davidclelland6@gmail.com>
2019-08-29 15:00:57 -0400

    commented code

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 //  bring in and require Mongoose to connect to MongoDB
 const mongoose = require('mongoose');
 
+//  bring in npm config
 const config = require('config');
+
 // set variable db as mongoURI string in default.json file
 const db = config.get('mongoURI');
 
 // TODO reseaarch further async and await and comment below function
+// connectDB variable with async & await/try & catch
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,28 @@
+//  import jwt (JSON Web Token)
 const jwt = require('jsonwebtoken');
+// Import Config npm
 const config = require('config');
 
+//  export function 
 module.exports = function (req, res, next) {
+
   // Get token from header
   const token = req.header('x-auth-token');
 
-  // Check if no token
+  // Check if no token - if there is no token sent back from JWT respond with status 401 and message
   if (!token) {
     return res.status(401).json({
       msg: 'No token, authorization denied'
     });
   }
 
-  // Verify Token
+  // Verify Token with try/catch
   try {
+    // decoded variable jwt method .verify, pass in token and use config to get jwtSecrect from default.json
     const decoded = jwt.verify(token, config.get('jwtSecret'));
-
+    // request user 
     req.user = decoded.user;
+    // once completed move to next (is this required with a try/catch??)
     next();
   } catch (err) {
     res.status(401).json({
