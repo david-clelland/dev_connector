058fa37f8cb89e8d0a8b68dce7923be5fb24b749
David <davidclelland6@gmail.com>
2019-09-18 15:46:31 -0400

    added ProfileTop & ProfileAbout components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,23 +4,31 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Spinner from '../layout/Spinner'
 import { getProfileById } from '../../actions/profile'
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 
 const Profile = ({ getProfileById, profile: { loading, profile }, auth, match }) => {
     useEffect(() => {
         getProfileById(match.params.id);
-    }, [getProfileById]);
+    }, [getProfileById, match.params.id]);
 
     return (
         <Fragment>
-            {profile === null || loading ? <Spinner /> : <Fragment>
+            {profile === null || loading ? (<Spinner />) : (<Fragment>
                 <Link to='/profiles' className='btn btn-light'>
                     back to profiles
                 </Link>
-                {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && <Link to='/edit-profile' className='btn btn-dark'>
+                {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>
                     Edit Profile
-                </Link>}
-            </Fragment>}
+                </Link>
+                )}
+                <div className="profile-grid my-1">
+                    <ProfileTop profile={profile} />
+                    <ProfileAbout profile={profile} />
+                </div>
+            </Fragment>
+            )}
         </Fragment>
     )
 }
