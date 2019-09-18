c6a435e7f962c5ab2c48611649790c92eb65e908
David <davidclelland6@gmail.com>
2019-09-18 11:17:12 -0400

    get github repos action, reducer, type

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -7,7 +7,8 @@ import {
     PROFILE_ERROR,
     UPDATE_PROFILE,
     CLEAR_PROFILE,
-    ACCOUNT_DELETED
+    ACCOUNT_DELETED,
+    GET_REPOS,
 } from './types';
 
 // Get current users profile
@@ -61,6 +62,25 @@ export const getProfileById = userId => async dispatch => {
         });
     }
 };
+
+// Get Github repos
+export const getGithubRepos = username => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`)
+
+        dispatch({
+            type: GET_REPOS,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+
 
 
 //  Create or Update a Profile
