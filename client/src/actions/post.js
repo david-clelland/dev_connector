0e299e60e559d3c6539a3dc95186426cf9de366d
David <davidclelland6@gmail.com>
2019-09-19 10:31:34 -0400

    like & unlike functionality for posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES,
+
 } from './types'
 
 // Get posts
@@ -20,4 +22,38 @@ export const getPosts = () => async dispatch => {
             payload: { msg: err.response.statusText, status: err.response.status }
         });
     }
-}
+};
+
+// Add Like
+export const addLike = id => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/like/${id}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Remove Like
+export const removeLike = id => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/unlike/${id}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
